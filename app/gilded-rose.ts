7abfd14dc6a47f53a1ea87d4bbd0311c7cb67e77
ng3w-uv <ng3w.uv@gmail.com>
2023-03-24 09:07:33 +0530

    1st iteration of refactoring the code and added an array test

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -10,6 +10,42 @@ export class Item {
   }
 }
 
+const agedBrie = "Aged Brie";
+const backstagePasses = "Backstage passes";
+const sulfuras = "Sulfuras";
+
+let itemIsNotUnique = (x) => !x.name.includes(agedBrie) && !x.name.includes(backstagePasses) && !x.name.includes(sulfuras);
+
+let itemIsAgedBrie = (x) => x.name.includes(agedBrie);
+let itemIsBackstagePasses = (x) => x.name.includes(backstagePasses);
+let itemIsSulfuras = (x) => x.name.includes(sulfuras);
+
+let singleSellInDecrement = (x) => (x.sellIn = x.sellIn - 1);
+
+let singleQualityDecrement = (x) => {
+  if (x.quality > 0) x.quality = x.quality - 1;
+  else x.quality = 0;
+};
+
+let doubleQualityDecrement = (x) => {
+  if (x.quality > 1) x.quality = x.quality - 2;
+  else x.quality = 0;
+};
+
+let singleQualityIncrement = (x) => {
+  if (x.quality < 50) x.quality = x.quality + 1;
+  else x.quality = 50;
+};
+
+let doubleQualityIncrement = (x) => {
+  if (x.quality < 49) x.quality = x.quality + 2;
+  else x.quality = 50;
+};
+
+let trippleQualityIncrement = (x) => {
+  if (x.quality < 48) x.quality = x.quality + 3;
+  else x.quality = 50;
+};
 export class GildedRose {
   items: Array<Item>;
 
@@ -18,51 +54,33 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
+    this.items.map((item) => {
+      if (itemIsNotUnique(item)) {
+        if (item.sellIn > 0) singleQualityDecrement(item);
+        else doubleQualityDecrement(item);
+        singleSellInDecrement(item);
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+    });
+
+    this.items.map((item) => {
+      if (itemIsAgedBrie(item)) {
+        if (item.sellIn > 0) singleQualityIncrement(item);
+        else doubleQualityIncrement(item);
+        singleSellInDecrement(item);
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
+    });
+
+    this.items.map((item) => {
+      if (itemIsBackstagePasses(item)) {
+        if (item.sellIn <= 0) item.quality = 0;
+        else if (item.sellIn <= 10 && item.sellIn > 5)
+          doubleQualityIncrement(item);
+        else if (item.sellIn > 0 && item.sellIn <= 5)
+          trippleQualityIncrement(item);
+        else singleQualityIncrement(item);
+        singleSellInDecrement(item);
       }
-    }
+    });
 
     return this.items;
   }
--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -23,26 +23,10 @@ describe("Gilded Rose Class Assigning", () => {
         const gildedRose = new GildedRose([new Item("", 0, 3)]);
         expect(gildedRose.items[0].quality).toBe(3);
     });
-    
+
 });
 
 describe("GildedROse sample tests", () => {
-
-    it('what if the quality provided is negative for a sample item', () => {
-        const gildedRose = new GildedRose([new Item("sample_item", 10, -23)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toBe("sample_item");
-        expect(items[0].sellIn).toBe(9);
-        expect(items[0].quality).toBe(-23);
-    });
-    
-    it('what if the quality provided is negative for Aged Brie /special item', () => {
-        const gildedRose = new GildedRose([new Item("Aged Brie", 10, -23)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toBe("Aged Brie");
-        expect(items[0].sellIn).toBe(9);
-        expect(items[0].quality).toBe(-22);
-    });
 
     it("SellIn date has passed quality degrades twice as fast", () => {
         const gildedRose = new GildedRose([new Item("test_item", -1, 20)]);
@@ -82,8 +66,8 @@ describe("GildedROse sample tests", () => {
         expect(items[0].name).toBe("Aged Brie");
         expect(items[0].sellIn).toBe(9);
         expect(items[0].quality).toBe(26);
-     });  
-     
+     });
+
     it('correct assigning of Sulfuras\'s quality and its change even SellIn date does not change', () => {
         const gildedRose = new GildedRose([new Item("Sulfuras, Hand of Ragnaros", -10, 80)]);
         const items = gildedRose.updateQuality();
@@ -99,7 +83,7 @@ describe("GildedROse sample tests", () => {
         expect(items[0].sellIn).toBe(9);
         expect(items[0].quality).toBe(37);
     });
-    
+
     it('Backstage passes when 5 days or less are left', () => {
         const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 35)]);
         const items = gildedRose.updateQuality();
@@ -107,7 +91,7 @@ describe("GildedROse sample tests", () => {
         expect(items[0].sellIn).toBe(4);
         expect(items[0].quality).toBe(38);
     });
-    
+
     it('Backstage passes when sellIn date is 0', () => {
         const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 35)]);
         const items = gildedRose.updateQuality();
@@ -116,7 +100,6 @@ describe("GildedROse sample tests", () => {
         expect(items[0].quality).toBe(0);
     });
 
-    
     it('Backstage passes when sellIn date has passed', () => {
         const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", -2, 35)]);
         const items = gildedRose.updateQuality();
@@ -124,11 +107,33 @@ describe("GildedROse sample tests", () => {
         expect(items[0].sellIn).toBe(-3);
         expect(items[0].quality).toBe(0);
     });
-    
-
-
-
-
 
 });
 
+describe("Array tests", () => {
+  it("checking Array items", () => {
+    let itemList: Item[] = [
+      { name: "sample_item", sellIn: 10, quality: 30 },
+      { name: "Aged Brie", sellIn: 10, quality: 30 },
+      { name: "Aged Brie Wine", sellIn: 0, quality: 30 },
+      { name: "Backstage passes to a Badshah concert", sellIn: 0, quality: 30 },
+      { name: "Backstage passes to a Honey Singh concert", sellIn: 9, quality: 30 },
+      { name: "Backstage passes to a Raftaar concert", sellIn: 3, quality: 30 },
+      { name: "Backstage passes to a MC Stan concert", sellIn: 15, quality: 30 },
+      { name: "Sulfuras the God element", sellIn: 10, quality: 80 },
+    ];
+
+    const arrayItems = new GildedRose(itemList);
+    // const gildedRose = new GildedRose([new Item("sam", 10, -23)],[new Item("sam", 10, -23)]);
+
+    const items = arrayItems.updateQuality();
+    expect(items[0]).toStrictEqual({name: "sample_item",sellIn: 9,quality: 29});
+    expect(items[1]).toStrictEqual({name: "Aged Brie",sellIn: 9, quality: 31});
+    expect(items[2]).toStrictEqual({name: "Aged Brie Wine",sellIn: -1, quality: 32});
+    expect(items[3]).toStrictEqual({name: "Backstage passes to a Badshah concert",sellIn: -1, quality: 0});
+    expect(items[4]).toStrictEqual({name: "Backstage passes to a Honey Singh concert",sellIn: 8, quality: 32});
+    expect(items[5]).toStrictEqual({name: "Backstage passes to a Raftaar concert",sellIn: 2, quality: 33});
+    expect(items[6]).toStrictEqual({name: "Backstage passes to a MC Stan concert",sellIn: 14, quality: 31});
+    expect(items[7]).toStrictEqual({ name: "Sulfuras the God element", sellIn: 10, quality: 80 });
+  });
+});
